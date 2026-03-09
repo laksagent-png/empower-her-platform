1118a27951646a54c1715b1eee89d380b36da5d2
Lakshya Singla <laks.agent@gmail.com>
2026-03-09 11:59:30 +0530

    added base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/empower-her-platform/',
   server: {
     host: "::",
     port: 8080,
